e2a7e7cf9e900c511955639b79d659dc83980da3
Iman <imanghafoori1@gmail.com>
2022-01-19 19:09:22 +0330

    Update Middleware.php

--- src/Attribute/Middleware.php
+++ src/Attribute/Middleware.php
@@ -6,16 +6,17 @@ use Attribute;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Middleware
-{
+{    
+    public array $options = [];
+
     /**
      * @param string $name
      * @param string|object|string[]|object[] $arguments
      * @param string|string[] $only
      * @param string|string[] $except
      */
-    function __construct(public string $name, public string | object | array $arguments = [], public string | array $only = [], public string | array $except = [])
+    public function __construct(public string $name, public string | object | array $arguments = [], public string | array $only = [], public string | array $except = [])
     {
+        //
     }
-
-    public array $options = [];
 }
